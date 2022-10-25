5eb7837bc79672f4cd7bd782c4e7d9742d4e05ac
Ruoyu Chen <39595892+RuoyuChen10@users.noreply.github.com>
2022-10-25 16:53:46 +0800

    Update zsd.py

--- zsd.py
+++ zsd.py
@@ -26,11 +26,10 @@ config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 # 从 model zoo 下载 checkpoint 并放在 `checkpoints/` 文件下
 # 网址为: http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
 checkpoint_file = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-device = 'cuda:0'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def init():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
     # 初始化检测器
     detector_model = init_detector(config_file, checkpoint_file, device=device)
 
